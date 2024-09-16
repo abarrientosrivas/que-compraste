1bf2d16bfcf95cac796d690f4e9726391ad52124
abarrientosrivas <alexisbarrientos97@gmail.com>
2024-09-16 12:24:23 -0300

    offloaded message processing to new thread

--- PyMessaging/typed_messaging.py
+++ PyMessaging/typed_messaging.py
@@ -2,10 +2,13 @@ from pika.adapters.blocking_connection import BlockingChannel
 from pika.exceptions import ChannelClosedByBroker
 from pika.spec import Basic, BasicProperties
 from pika import BlockingConnection, PlainCredentials, ConnectionParameters
-import json, time
 from urllib.parse import urlparse
 from pydantic import BaseModel
 from typing import Callable, Type, Any
+import json
+import time
+import threading
+import queue
 
 def bind_queue(queue_name: str, exchange_name: str, routing_key: str | None, conn: BlockingConnection):
     channel = conn.channel()
@@ -48,6 +51,7 @@ def get_connection_by_string(conn_string:str) -> BlockingConnection:
 class PydanticQueueConsumer:
     def __init__(self, channel: BlockingChannel):
         self.channel = channel
+        self.message_queue = queue.Queue()
 
     def on_message(
             self,
@@ -55,30 +59,50 @@ class PydanticQueueConsumer:
             method_frame: Basic.Deliver, 
             header_frame: BasicProperties, 
             body: bytes):
-        try:
-            json_data = json.loads(body.decode())
-            obj = self.expected_type(**json_data)
-            self.callback(obj)
-
-            channel.basic_ack(method_frame.delivery_tag)
-        except Exception as e:
-            if self.error_callback:
-                self.error_callback(e)
-            channel.basic_nack(method_frame.delivery_tag, requeue=False)
+        self.message_queue.put((channel, method_frame, body))
 
     def start(self, queue_name: str, callback: Callable[[BaseModel], Any], expected_type: Type[BaseModel], error_callback: Callable[[Exception], Any] | None = None):
         self.should_consume = True
         self.expected_type = expected_type
         self.callback = callback
         self.error_callback = error_callback
+
+        self.worker_thread = threading.Thread(target=self.process_messages)
+        self.worker_thread.start()
+
         self.channel.basic_consume(queue_name, self.on_message)
         self.channel.basic_qos(prefetch_count=1)
+
         while self.should_consume:
             self.channel.connection.process_data_events(time_limit=1)
             time.sleep(0.1)
 
+    def process_messages(self):
+        while self.should_consume:
+            try:
+                channel, method_frame, body = self.message_queue.get(timeout=1)
+                self.process_message(channel, method_frame, body)
+            except queue.Empty:
+                continue
+
+    def process_message(self, channel, method_frame, body):
+        try:
+            json_data = json.loads(body.decode())
+            obj = self.expected_type(**json_data)
+            self.callback(obj)
+            self.channel.connection.add_callback_threadsafe(
+                lambda: channel.basic_ack(method_frame.delivery_tag)
+            )
+        except Exception as e:
+            if self.error_callback:
+                self.error_callback(e)
+            self.channel.connection.add_callback_threadsafe(
+                lambda: channel.basic_nack(method_frame.delivery_tag, requeue=False)
+            )
+
     def stop(self):
         self.should_consume = False
+        self.worker_thread.join()
         self.channel.stop_consuming()
 
 class PydanticExchangePublisher:
