d48f7f0b6790dbf81287572022b3fe91ab372a1d
Ariel Sepulveda <ariel.sepulveda.ar@gmail.com>
2024-09-22 23:44:46 -0300

    Agrega pagina inicio

--- web-app/src/app/app.routes.ts
+++ web-app/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 import { TicketImageUploadComponent } from './ticket-image-upload/ticket-image-upload.component';
 
 export const routes: Routes = [
+  { path: '', component: InicioComponent },
   { path: 'upload', component: TicketImageUploadComponent },
 ];
