d307f833fa966163cb5c0b3a6df0ef382f6e2042
abarrientosrivas <alexisbarrientos97@gmail.com>
2024-10-06 23:41:39 -0300

    qwen2 vl llm node for receipt reading

--- PyNodes/qwen_receipt_reader_node.py
+++ PyNodes/qwen_receipt_reader_node.py
@@ -4,28 +4,28 @@ import logging
 import threading
 import argparse
 import logging
+import json
 from API.schemas import ReceiptImageLocation
 from pydantic import ValidationError
 from json.decoder import JSONDecodeError
 from PyLib import typed_messaging, request_tools
 from dotenv import load_dotenv
-from huggingface_hub import HfFolder
 from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
 from qwen_vl_utils import process_vision_info
+from PyLib.donut_model import convert_data_to_purchase
+from PIL import Image
+from io import BytesIO
 
 load_dotenv()
-hf_token = os.getenv('HF_TOKEN')
-if hf_token is None or not hf_token.strip():
-    logging.error(f"No Hugging Face token was provided")
-    raise
-HfFolder.save_token(hf_token)
 
 class ImageToCompraNode:
-    def __init__(self, consumer: typed_messaging.PydanticQueueConsumer, publisher: typed_messaging.PydanticExchangePublisher, input_queue: str, output_endpoint: str):
+    def __init__(self, node_token: str, receipt_output_path: str, consumer: typed_messaging.PydanticQueueConsumer, publisher: typed_messaging.PydanticExchangePublisher, input_queue: str, output_endpoint: str):
         self.consumer = consumer
         self.publisher = publisher
         self.input_queue = input_queue
         self.output_endpoint = output_endpoint
+        self.node_token = node_token
+        self.receipt_output_path = receipt_output_path
         self.stop_event = threading.Event()
         self.model = Qwen2VLForConditionalGeneration.from_pretrained(
             "Qwen/Qwen2-VL-7B-Instruct", torch_dtype="auto", device_map="auto"
@@ -35,8 +35,20 @@ class ImageToCompraNode:
     def callback(self, message: ReceiptImageLocation):
         if message.path.strip():
             logging.info(f"Processing image with path: {message.path}")
+            receipt_path = message.path
         elif message.url.strip():
             logging.info(f"Processing image with url: {message.url}")
+            headers = {
+                "Authorization": f"Bearer {self.node_token}"
+            }
+            response = request_tools.send_request_with_retries('get', message.url, headers= headers)
+            if response.status_code != 200:
+                raise Exception(f"Failed to load image from URL: {response.status_code}")
+            
+            receipt_path = self.receipt_output_path
+
+            with open(receipt_path, 'wb') as f:
+                f.write(response.content)
         else:
             logging.warning("Received an empty message. Skipping...")
             return
@@ -47,9 +59,96 @@ class ImageToCompraNode:
                 "content": [
                     {
                         "type": "image",
-                        "image": message.path,
+                        "image": receipt_path,
                     },
-                    {"type": "text", "text": "Describe this image."},
+                    {
+                        "type": "text", 
+                        "text": 
+'''Read the following receipt image and return it's text as clean JSON, and only JSON, under the following schema:
+{
+    "type": "object",
+    "properties": {
+        "store_name": {
+            "type": "string",
+            "description": "The brand name of the store where the purchase was made."
+        },
+        "store_addr": {
+            "type": "string",
+            "description": "The address of the store."
+        },
+        "entity_id": {
+            "type": "string",
+            "description": "The tax or business identification number of the store."
+        },
+        "phone": {
+            "type": "string",
+            "description": "The contact phone number of the store."
+        },
+        "date": {
+            "type": "string",
+            "description": "The date of the transaction."
+        },
+        "time": {
+            "type": "string",
+            "description": "The time of the transaction."
+        },
+        "subtotal": {
+            "type": "string",
+            "description": "The subtotal amount of the purchase."
+        },
+        "svc": {
+            "type": "string",
+            "description": "Service charges if applicable."
+        },
+        "tax": {
+            "type": "string",
+            "description": "The tax amount."
+        },
+        "total": {
+            "type": "string",
+            "description": "The total amount paid."
+        },
+        "tips": {
+            "type": "string",
+            "description": "The tips amount if any."
+        },
+        "discount": {
+            "type": "string",
+            "description": "Total discount applied to the transaction."
+        },
+        "line_items": {
+            "type": "array",
+            "description": "List of items purchased.",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "item_key": {
+                        "type": "string",
+                        "description": "Unique identifier for the item."
+                    },
+                    "item_name": {
+                        "type": "string",
+                        "description": "Text description of the item."
+                    },
+                    "item_value": {
+                        "type": "string",
+                        "description": "Price of a single unit of the item."
+                    },
+                    "item_quantity": {
+                        "type": "string",
+                        "description": "Quantity of the item purchased."
+                    },
+                    "item_total": {
+                        "type": "string",
+                        "description": "Total amount charged for this purchase's item."
+                    }
+                }
+            }
+        }
+    }
+}
+'''
+                    },
                 ],
             }
         ]
@@ -68,27 +167,28 @@ class ImageToCompraNode:
         inputs = inputs.to("cuda")
 
         # Inference: Generation of the output
-        generated_ids = self.model.generate(**inputs, max_new_tokens=128)
+        generated_ids = self.model.generate(**inputs, max_new_tokens=2048)
         generated_ids_trimmed = [
             out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
         ]
         output_text = self.processor.batch_decode(
             generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
         )
-        print(output_text)
+        json_str = output_text.removeprefix("```json").removesuffix("```").strip()
+        try:
+            json_data = json.loads(json_str)
+        except json.JSONDecodeError as e:
+            logging.error(f"Error decoding JSON: {e}")
+            return
 
-        # json_data = self.donut.read_receipt_image(pixel_values)
-        # if not json_data or not isinstance(json_data, dict):
-        #     logging.error("Could not generate proper JSON from image")
-        #     return
-        # purchase = convert_data_to_purchase(json_data)
-        # purchase_data = purchase.model_dump(mode='json')
+        purchase = convert_data_to_purchase(json_data)
+        purchase_data = purchase.model_dump(mode='json')
 
-        # response =  request_tools.send_request_with_retries("post", self.output_endpoint, purchase_data, stop_event= self.stop_event)
-        # if response.status_code == 200:
-        #     logging.info("Purchase created successfully")
-        # else:
-        #     logging.error(f"Failed to create purchase. Status code: {response.status_code}. Server response: {response.text}")
+        response =  request_tools.send_request_with_retries("post", self.output_endpoint, purchase_data, stop_event= self.stop_event)
+        if response.status_code == 200:
+            logging.info("Purchase created successfully")
+        else:
+            logging.error(f"Failed to create purchase. Status code: {response.status_code}. Server response: {response.text}")
         
     def error_callback(self, error: Exception):        
         if isinstance(error, JSONDecodeError):
@@ -121,8 +221,19 @@ if __name__ == '__main__':
     if not endpoint_url:
         logging.error("Endpoint URL was not provided")
         sys.exit(1)
+    
+    endpoint_url = os.getenv('IMAGE_TO_COMPRA_TOKEN')
+    if not endpoint_url:
+        logging.error("Node token was not provided")
+        sys.exit(1)
+    
+    endpoint_url = os.getenv('IMAGE_TO_COMPRA_RECEIPT_PATH')
+    if not endpoint_url:
+        logging.error("Receipt path was not provided")
+        sys.exit(1)
 
     node = ImageToCompraNode(
+        
         broker.get_consumer(), 
         broker.get_publisher(), 
         broker.ensure_queue(os.getenv('IMAGE_TO_COMPRA_INPUT_QUEUE', '')), 
