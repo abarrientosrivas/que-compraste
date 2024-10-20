bc2607845a3f9767f4b498ce9e8406be139d1039
Ariel Sepulveda <ariel.sepulveda.ar@gmail.com>
2024-10-19 23:40:20 -0300

    removed edit on purchase view

--- web-app/src/app/purchases/purchase/compra.component.ts
+++ web-app/src/app/purchases/purchase/compra.component.ts
@@ -18,13 +18,11 @@ export class CompraComponent {
   private activatedRoute = inject(ActivatedRoute);
   compraId = this.activatedRoute.snapshot.params['compraId'];
   compra: any;
-  compra_backup: any;
-  isEditMode = false;
-  selectedItem: any; // editar producto
 
   ngOnInit() {
     this.comprasService.getCompraById(this.compraId).subscribe({
-      next: (data) => {
+      next: (data: any) => {
+        data.items.sort((a: any, b: any) => a.id - b.id);
         this.compra = data;
         console.log('Respuesta del servidor:', data);
       },
@@ -37,27 +35,8 @@ export class CompraComponent {
     });
   }
 
-  editarCompra() {
-    this.compra_backup = JSON.parse(JSON.stringify(this.compra));
-    this.isEditMode = true;
-  }
-
   convertToInt(value: string): number {
     return parseInt(value, 10);
-  }
-
-  onProductSelected(event: any) {
-    const index = this.compra.items.findIndex(
-      (item: any) => item === this.selectedItem
-    );
-    if (index !== -1) {
-      console.log('Elemento actual: ', this.selectedItem);
-      this.compra.items[index].read_product_text = event.product.title;
-      this.compra.items[index].read_product_key = event.code;
-      this.compra.items[index].product_id = event.id;
-    }
-
-    console.log('Evento recibido de product-search: ', event);
   }
 
   get fechaFormateada() {
@@ -77,47 +56,5 @@ export class CompraComponent {
       hour: 'numeric',
       minute: 'numeric',
     });
-  }
-
-  guardarCambios() {
-    console.log('Cambios guardados:', this.compra);
-    this.comprasService.updateCompra(this.compra.id, this.compra).subscribe({
-      next: (data) => {
-        console.log('Respuesta del servidor:', data);
-        //this.compra = data; cuando se actualice la compra en el backend descomentar
-      },
-      error: (error) => {
-        console.error('Error al hacer la petición:', error);
-      },
-      complete: () => {
-        console.log('Petición completada');
-      },
-    });
-    this.isEditMode = false;
-  }
-
-  cancelar() {
-    this.isEditMode = false;
-    this.compra = this.compra_backup;
-  }
-
-  seleccionarItem(item: any) {
-    this.selectedItem = item;
-  }
-
-  onValueChange(value: number, index: number) {
-    this.compra.items[index].total = value * this.compra.items[index].quantity;
-    this.compra.total = this.compra.items.reduce(
-      (sum: any, item: { total: any }) => sum + item.total,
-      0
-    );
-  }
-
-  onQuantityChange(quantity: number, index: number) {
-    this.compra.items[index].total = quantity * this.compra.items[index].value;
-    this.compra.total = this.compra.items.reduce(
-      (sum: any, item: { total: any }) => sum + item.total,
-      0
-    );
   }
 }
